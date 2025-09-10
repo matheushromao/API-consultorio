cd4e4c6e84819bd19f76ef95a4cbf37c2f184b7d
Matheus Romão <romaomatheus568@gmail.com>
2025-09-10 19:15:21 -0300

    att da parte anterior

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,12 @@ import { Express, Request, Response } from "express";
 const app: Express = express();
 const port: number = 3000; 
 
+app.use(express.json());
 
 app.get("/", (req, res) => {
     console.log("Hello world!");
 })
 
-app.listen()
+app.listen(port, () => {
+    console.log(`A API está rodando na ${port}`);
+})
