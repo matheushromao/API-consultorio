66100c261cdf28b9537871b8db711d8d87fb40f5
Matheus Romão <matheus_henrique_9@hotmail.com>
2025-09-18 10:00:34 -0300

    Melhorando a forma de importar os express

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import express from "express";
-import { Express, Request, Response } from "express";
+import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
 const port: number = 3000; 
