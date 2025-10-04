2f4d4862ec3f1181b223d72c493ad59be82ea0f6
Matheus Romão <matheus_henrique_9@hotmail.com>
2025-10-04 16:28:23 -0300

    ajeitando o index e o swagger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
-
+import { setupSwagger } from "./swagger";
 import routes from "./routes";
+import "dotenv/config";
 
 const app = express();
 
 app.use(express.json());
-
+setupSwagger(app);
 app.use("", routes);
 
 const PORT = process.env.PORT || 3000;
---
+++ src/swagger.ts
@@ -0,0 +1,27 @@
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { Express } from "express";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API de Consultório Médico",
+      version: "1.0.0",
+      description:
+        "API para gerenciar consultas, médicos, pacientes e secretários",
+    },
+    servers: [
+      {
+        url: "http://localhost:3333",
+      },
+    ],
+  },
+  apis: ["./src/routes/*.ts"],
+};
+
+const specs = swaggerJsdoc(options);
+
+export const setupSwagger = (app: Express) => {
+  app.use("/api-fisio", swaggerUi.serve, swaggerUi.setup(specs));
+};
